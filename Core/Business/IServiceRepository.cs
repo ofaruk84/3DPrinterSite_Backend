7ede3e4aa5386d59f0b15d8becc69547a7e701d9
Ömer Faruk Korkmaz <korkmazfaruk84@gmail.com>
2021-03-31 22:19:10 +0300

    Core Added

---
+++ Core/Business/IServiceRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Business
+{
+    public interface IServiceRepository<T>
+    {
+        IDataResult<List<T>> GetAll();
+        IDataResult<T> GetById(int id);
+        IResult Add(T entity);
+        IResult Update(T entity);
+        IResult Delete(T entity);
+
+   
+
+    }
+}
---
+++ Core/CrossCuttingConcerns/Validations/ValidationTool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+using Microsoft.Extensions.Options;
+
+namespace Core.CrossCuttingConcerns.Validations
+{
+    public static class ValidationTool
+    {
+        public static void ValidateOptions(IValidator validator,object entity)
+        {
+
+        }
+    }
+}
