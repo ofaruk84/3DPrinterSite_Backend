36be6479f86079d6cc7a4b01a77e2fca228b39fa
Resul Ekinci <resulekinci10@gmail.com>
2021-04-12 01:05:47 +0300

    All Entities has been written. DataAccess has been written. EntityFramework  has been entegrated.

---
+++ DataAccess/Concrete/EntityFramework/EfSellerDal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfSellerDal : ISellerDal
+    {
+        public void Add(Seller entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Seller entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Seller Get(Expression<Func<Seller, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Seller> GetAll(Expression<Func<Seller, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Seller entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Entities/Concrete/Seller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Entities.Concrete
+{
+    public class Seller:IEntity
+    {
+        public int SellerId { get; set; }
+        public int UserId { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyAdress { get; set; }
+        public string  CompanyPhone { get; set; }
+
+    }
+}
