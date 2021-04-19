bc5c3485cb64fa3a015a3f64eedc24f1649cb95b
mdenizsert <e170503010@stud.tau.edu.tr>
2021-04-19 16:11:49 +0300

    Validation System base created. Some validation rules for users created. Neccesary methods for ValidationTool in Core written.

---
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidator:AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty();
+            RuleFor(p => p.Surname).NotEmpty();
+            //RuleFor(p => p.Password).MinimumLength(8);
+        }
+    }
+}
